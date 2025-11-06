ccef0ae60cd1ea3c35dacf523bd7abccdff8c30f
Krealiss <astorthak@gmail.com>
2025-11-06 17:33:24 +0200

    Part1

---
+++ lb2/part1.py
@@ -0,0 +1,20 @@
+﻿import hashlib
+
+text = input("Enter a string: ")
+
+# SHA-256 calculation
+sha256_hash = hashlib.sha256(text.encode()).hexdigest()
+
+# SHA-3 (256) calculation
+sha3_256_hash = hashlib.sha3_256(text.encode()).hexdigest()
+
+# Results display
+print("\nResults:")
+print(f"SHA-256:   {sha256_hash}")
+print(f"SHA3-256:  {sha3_256_hash}")
+print(f"\nLength SHA-256:  {len(sha256_hash)} symbols")
+print(f"Length SHA3-256: {len(sha3_256_hash)} symbols")
+
+#Обидва хеші мають однакову довжину (256 біт або 64 шістнадцяткові символи).
+#SHA-256 — швидший у виконанні, часто використовується в Bitcoin.
+#SHA-3 — новіший стандарт, має іншу криптографічну структуру (Keccak) і вважається надійнішим.
